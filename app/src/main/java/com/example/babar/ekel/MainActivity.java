fa05ce0a5d5e412164727fe398f45d7ca8998714
root <root@DESKTOP-CK8RFD6.localdomain>
2017-12-03 23:56:23 +0100

    For me is done

--- app/src/main/java/com/example/babar/ekel/MainActivity.java
+++ app/src/main/java/com/example/babar/ekel/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.babar.ekel;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity implements ListFragment.OnItemClick {
@@ -19,12 +24,27 @@ public class MainActivity extends FragmentActivity implements ListFragment.OnIte
 
     @Override
     public void onSentText(DataModel model) {
-        Intent intent = new Intent(this, OrderActivity.class);
-        intent.putExtra("NAME", model.getName());
-        intent.putExtra("ID", model.getMealId());
-        intent.putExtra("IMAGE", model.getImageID());
-        intent.putExtra("PRICE", model.getPrice());
-        Toast.makeText(this, "startuje nowa intetn", Toast.LENGTH_SHORT).show();
-        startActivityForResult(intent, RETURN_RATING);
+        if(model.isAvailable()) {
+            Intent intent = new Intent(this, OrderActivity.class);
+            intent.putExtra("NAME", model.getName());
+            intent.putExtra("ID", model.getMealId());
+            intent.putExtra("IMAGE", model.getImageID());
+            intent.putExtra("PRICE", model.getPrice());
+            startActivityForResult(intent, RETURN_RATING);
+        } else {
+            LayoutInflater inflater = getLayoutInflater();
+            View layout = inflater.inflate(R.layout.custom_toast,
+                    (ViewGroup) findViewById(R.id.custom_toast_container));
+
+            TextView text = layout.findViewById(R.id.text);
+            text.setText(R.string.error1);
+
+            Toast toast = new Toast(getApplicationContext());
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.setDuration(Toast.LENGTH_LONG);
+            toast.setView(layout);
+            toast.show();
+
+        }
     }
 }
--- app/src/main/java/com/example/babar/ekel/OrderActivity.java
+++ app/src/main/java/com/example/babar/ekel/OrderActivity.java
@@ -5,20 +5,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class OrderActivity extends AppCompatActivity {
 
     public static final String BASE_URL = "http://10.0.2.2:8080/";
-    private EditText table_number;
+    private Spinner table_number;
     private int meal_id;
+    private ArrayAdapter<CharSequence> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +40,9 @@ public class OrderActivity extends AppCompatActivity {
         double price = b.getDouble("PRICE");
 
         table_number = findViewById(R.id.table_number);
+        adapter = ArrayAdapter.createFromResource(this, R.array.tables, R.layout.spinner_item);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        table_number.setAdapter(adapter);
 
         ImageView imageView = findViewById(R.id.order_image);
         TextView tv_name = findViewById(R.id.order_name);
@@ -40,23 +50,39 @@ public class OrderActivity extends AppCompatActivity {
 
         imageView.setImageResource(resourceImg);
         tv_name.setText(name);
-        tv_price.setText(String.valueOf(price));
+        String to_price = "Cena: " + String.valueOf(price)+" zł";
+        tv_price.setText(to_price);
 
         Button send_order = findViewById(R.id.order_button);
         send_order.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String customer_id = String.valueOf(table_number.getText());
+                String customer_id = table_number.getSelectedItem().toString();
 
-                Ion.with(getApplicationContext()).load(BASE_URL+"add/order/"+customer_id+"/"+meal_id).asString().setCallback(new FutureCallback<String>() {
+                Ion.with(getApplicationContext())
+                        .load(BASE_URL+"add/order/"+customer_id+"/"+meal_id)
+                        .asString().setCallback(new FutureCallback<String>() {
                     @Override
                     public void onCompleted(Exception e, String result) {
-                        Toast.makeText(getApplicationContext(),result, Toast.LENGTH_SHORT).show();
+                        try {
+                            JSONObject json = new JSONObject(result);
+                            Toast.makeText(getApplicationContext(),result , Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e1) {
+                            Toast.makeText(getApplicationContext(),"Dodano zamówienie" , Toast.LENGTH_SHORT).show();
+                            e1.printStackTrace();
+                            end();
+                        }
+
+
                     }
                 });
             }
         });
 
+    }
+
+    void end(){
+        finish();
     }
 
 
